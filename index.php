5209af5a2fdb9de43ec2d5c073c9df6e41e25869
kentos223 <159707649+kentos223@users.noreply.github.com>
2025-12-02 14:41:45 +0700

    Tes Otomatis

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
-echo "--------------------------------------------------\n";
-echo "STATUS: TUGAS OTOMASI BERHASIL!\n";
-echo "Halo Jenkins, kode ini dikirim dari Git Laptop saya.\n";
-echo "--------------------------------------------------\n";
+// Pesan ini akan muncul di Console Output Jenkins untuk membuktikan PHP jalan
+echo "=================================================\n";
+echo "STATUS: Halaman Index Berhasil Dijalankan Jenkins\n";
+echo "=================================================\n";
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Sederhana</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex justify-content-center align-items-center vh-100 bg-light">
+
+    <div class="card shadow p-4" style="width: 400px;">
+        <h3 class="text-center mb-4">Tambah Data</h3>
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama Lengkap:</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama" required>
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Simpan Data</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
